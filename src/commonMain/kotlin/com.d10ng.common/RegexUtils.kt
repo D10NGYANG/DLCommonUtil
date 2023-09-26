5e1b25e499a0091466ba56f8394bb62355a21029
D10NGYANG <D10NGYANG@163.com>
2023-09-26 13:48:19 +0800

    perf: 优化正则匹配工具；

--- src/commonMain/kotlin/com.d10ng.common/RegexUtils.kt
+++ src/commonMain/kotlin/com.d10ng.common/RegexUtils.kt
@@ -15,18 +15,22 @@ val chineseRegex = "[\u4E00-\u9FA5]+".toRegex()
 // 中文数字英文正则表达式
 val chEnNumRegex = "[a-zA-Z0-9\u4E00-\u9FA5]+".toRegex()
 // 汉字和全角符号正则表达式
-val chSymbolRegex = "[\u3000-\u301e\u3021-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uffef\u4E00-\u9FA5]+".toRegex()
+val chSymbolRegex = "[\u00A4\u00A7-\u00A8\u00B0-\u00B1\u00B7\u00D7\u00E0-\u00E1\u00E8-\u00EA\u00EC-\u00ED\u00F2-\u00F3\u00F7\u00F9-\u00FA\u00FC\u0101\u0113\u011B\u012B\u0144\u0148\u014D\u016B\u01CE\u01D0\u01D2\u01D4\u01D6\u01D8\u01DA\u01DC\u0251\u0261\u02C7\u02C9\u0391-\u03A1\u03A3-\u03A9\u03B1-\u03C1\u03C3-\u03C9\u0401\u0410-\u044F\u0451\u2014\u2016\u2018-\u2019\u201C-\u201D\u2026\u2030\u2032-\u2033\u203B\u20AC\u2103\u2116\u2160-\u216B\u2170-\u2179\u2190-\u2193\u2208\u220F\u2211\u221A\u221D-\u221E\u2220\u2225\u2227-\u222B\u222E\u2234-\u2237\u223D\u2248\u224C\u2260-\u2261\u2264-\u2265\u226E-\u226F\u2299\u22A5\u2312\u2460-\u2469\u2474-\u249B\u2500-\u254B\u25A0-\u25A1\u25B2-\u25B3\u25C6-\u25C7\u25CB\u25CE-\u25CF\u2605-\u2606\u2640\u2642\u3000-\u3003\u3005\u3008-\u3011\u3013-\u3017\u3041-\u3093\u30A1-\u30F6\u3105-\u3129\u3220-\u3229\uE766-\uE76B\uE76D-\uE7BB\uE7C7-\uE7E1\uE7FE-\uE80F\uFE31\uFE33-\uFE44\uFF01-\uFF5E\uFFE0-\uFFE1\uFFE3\uFFE5\uff00-\uffff\u4E00-\u9FA5]+".toRegex()
+// 身份证正则表达式
+val idCardRegex = "^[1-9]\\d{5}(?:18|19|20)\\d{2}(?:0[1-9]|10|11|12)(?:0[1-9]|[1-2]\\d|30|31)\\d{3}[0-9Xx]$".toRegex()
+// 国内手机号码正则表达式
+val mobileNumberRegex = "^1[1-9]\\d{9}$".toRegex()
+// 邮箱正则表达式
+val emailRegex = "^[A-Za-z0-9\\u4e00-\\u9fa5]+@[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(?:\\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+\$".toRegex()
 
 /**
  * 利用正则表达式保留字符串
- * @receiver String
- * @param regStr String
- * @return String
+ * @receiver [String]
+ * @param regStr [String]
+ * @return [String]
  */
 @JsExport
-fun String.keepByRegexStr(regStr: String): String {
-    return keep(regStr.toRegex())
-}
+fun String.keepByRegexStr(regStr: String) = keep(regStr.toRegex())
 
 /**
  * 利用正则表达式保留字符串
@@ -34,18 +38,13 @@ fun String.keepByRegexStr(regStr: String): String {
  * @param reg [Regex]
  * @return [String]
  */
-fun String.keep(reg: Regex): String {
-    val ls = reg.findAll(this).map { it.value }
-    val text = StringBuilder("")
-    ls.forEach { text.append(it) }
-    return text.toString()
-}
+fun String.keep(reg: Regex) = reg.findAll(this).map { it.value }.joinToString("")
 
 /**
  * 利用正则表达式过滤字符串
- * @receiver String
- * @param regStr String
- * @return String
+ * @receiver [String]
+ * @param regStr [String]
+ * @return [String]
  */
 @JsExport
 fun String.filterByRegexStr(regStr: String) = filter(regStr.toRegex())
@@ -65,7 +64,7 @@ fun String.filter(reg: Regex) = reg.replace(this, "")
  * @return [Boolean]
  */
 @JsExport
-fun String.isIdCard(): Boolean = "(^\\d{15}$)|(^\\d{17}([0-9]|X)$)".toRegex().matches(this)
+fun String.isIdCard() = idCardRegex.matches(this)
 
 /**
  * 判断字符串是否符合国内手机号码规则
@@ -74,7 +73,7 @@ fun String.isIdCard(): Boolean = "(^\\d{15}$)|(^\\d{17}([0-9]|X)$)".toRegex().ma
  * @return [Boolean]
  */
 @JsExport
-fun String.isMobileNumber(): Boolean = "^1[1-9]\\d{9}$".toRegex().matches(this)
+fun String.isMobileNumber() = mobileNumberRegex.matches(this)
 
 /**
  * 判断字符串是否符合邮箱规则
@@ -83,7 +82,7 @@ fun String.isMobileNumber(): Boolean = "^1[1-9]\\d{9}$".toRegex().matches(this)
  * @return [Boolean]
  */
 @JsExport
-fun String.isEmail(): Boolean = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$".toRegex().matches(this)
+fun String.isEmail() = emailRegex.matches(this)
 
 /**
  * 判断字符串是否只有中英文数字，不包含特殊字符
@@ -92,7 +91,7 @@ fun String.isEmail(): Boolean = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]
  * @return [Boolean]
  */
 @JsExport
-fun String.isOnlyChEnNum(): Boolean = chEnNumRegex.matches(this)
+fun String.isOnlyChEnNum() = chEnNumRegex.matches(this)
 
 /**
  * 判断字符串是否只有英文数字，不包含特殊字符
@@ -101,7 +100,7 @@ fun String.isOnlyChEnNum(): Boolean = chEnNumRegex.matches(this)
  * @return [Boolean]
  */
 @JsExport
-fun String.isOnlyEnNum(): Boolean = numEnRegex.matches(this)
+fun String.isOnlyEnNum() = numEnRegex.matches(this)
 
 /**
  * 判断字符串是否只有汉字
@@ -110,7 +109,7 @@ fun String.isOnlyEnNum(): Boolean = numEnRegex.matches(this)
  * @return [Boolean]
  */
 @JsExport
-fun String.isOnlyChinese(): Boolean = chineseRegex.matches(this)
+fun String.isOnlyChinese() = chineseRegex.matches(this)
 
 /**
  * 判断字符串是否只有字母
@@ -119,7 +118,7 @@ fun String.isOnlyChinese(): Boolean = chineseRegex.matches(this)
  * @return [Boolean]
  */
 @JsExport
-fun String.isOnlyLetter(): Boolean = letterRegex.matches(this)
+fun String.isOnlyLetter() = letterRegex.matches(this)
 
 /**
  * 判断字符串是否只有数字
@@ -128,7 +127,7 @@ fun String.isOnlyLetter(): Boolean = letterRegex.matches(this)
  * @return [Boolean]
  */
 @JsExport
-fun String.isOnlyNumber(): Boolean = numberRegex.matches(this)
+fun String.isOnlyNumber() = numberRegex.matches(this)
 
 /**
  * 判断字符串是否只有代码
@@ -137,7 +136,7 @@ fun String.isOnlyNumber(): Boolean = numberRegex.matches(this)
  * @return [Boolean]
  */
 @JsExport
-fun String.isOnlyCode(): Boolean = codeRegex.matches(this)
+fun String.isOnlyCode() = codeRegex.matches(this)
 
 /**
  * 判断字符串是否只有汉字和全角符号
@@ -146,4 +145,4 @@ fun String.isOnlyCode(): Boolean = codeRegex.matches(this)
  * @return [Boolean]
  */
 @JsExport
-fun String.isOnlyChSymbol(): Boolean = chSymbolRegex.matches(this)
+fun String.isOnlyChSymbol() = chSymbolRegex.matches(this)
---
+++ src/jvmTest/kotlin/Test.kt
@@ -0,0 +1,101 @@
+import com.d10ng.common.*
+import kotlin.test.Test
+
+class Test {
+
+    data class CodePoint(val gbkCode: String, val gbkText: String, val uniCode: String, val uniText: String, var less: Int = 0)
+
+    @Test
+    fun test() {
+        val v = 0xA1 .. 0xA9
+        val h = 0xA1 .. 0xFE
+        val toHex: (ByteArray) -> String = { it.toHexString(false, uppercase = true) }
+        var l = mutableListOf<CodePoint>()
+        for (i in v) {
+            for (j in h) {
+                val gbkCode = byteArrayOf(i.toByte(), j.toByte())
+                val gbkText = gbkCode.decodeGBK()
+                val uniCode = gbkText.encodeUnicode()
+                val uniText = uniCode.decodeUnicodeByByteArray()
+                l.add(CodePoint(toHex(gbkCode), gbkText, toHex(uniCode), uniText))
+            }
+        }
+
+        l = l.sortedBy { it.uniCode }.toMutableList()
+        l.forEachIndexed { idx, p ->
+            p.less = if (idx == 0) 0 else p.uniCode.toByteArrayFromHex().toUnsignedInt() - l[idx - 1].uniCode.toByteArrayFromHex().toUnsignedInt()
+        }
+        l.forEach { p ->
+            println("GBK编码(${p.gbkCode}) 内容(${p.gbkText}) | Unicode编码(${p.uniCode}) 内容(${p.uniText}) | 差值(${p.less})")
+        }
+
+        val sb = StringBuilder()
+        l.forEachIndexed { idx, p ->
+            if (p.less != 1) {
+                sb.append("\\u${p.uniCode}")
+            } else if (idx != l.size -1 && l[idx + 1].less != 1) {
+                sb.append("-\\u${p.uniCode}")
+            } else if (idx == l.size - 1) {
+                sb.append("\\u${p.uniCode}")
+            }
+        }
+        println(sb.toString())
+    }
+
+    @Test
+    fun test1() {
+        println("".encodeGBK().toHexString(false, true))
+    }
+
+    @Test
+    fun test2() {
+        val v = 0x20 .. 0x7E
+        for (i in v) {
+            val asciiCode = byteArrayOf(i.toByte())
+            val asciiText = asciiCode.decodeASCII()
+            val uniCode = asciiText.encodeUnicode()
+            val uniText = uniCode.decodeUnicodeByByteArray()
+            val gbkCode = (uniCode.toUnsignedInt() + 0xFEE0).toByteArray().decodeUnicodeByByteArray().encodeGBK()
+            val gbkText = gbkCode.decodeGBK()
+            println("ASCII编码(${asciiCode.toHexString(false, uppercase = true)}) 内容(${asciiText}) | Unicode编码(${uniCode.toHexString(false, uppercase = true)}) 内容(${uniText}) | GBK编码(${gbkCode.toHexString(false, uppercase = true)}) 内容(${gbkText})")
+        }
+    }
+
+    @Test
+    fun test3() {
+        chSymbolRegex.findAll("测试123，阿说过的哈是觉得尬，阿嘉豪看帅哥带回家啊点").map { it.value }.joinToString("").let(::println)
+    }
+
+    @Test
+    fun test4() {
+        val v = 0x0080 .. 0x00ff
+        for (i in v) {
+            val asciiCode = byteArrayOf(i.toByte())
+            val asciiText = asciiCode.decodeASCII()
+            val uniCode = asciiText.encodeUnicode()
+            val uniText = uniCode.decodeUnicodeByByteArray()
+            val gbkCode = asciiText.encodeGBK()
+            val gbkText = gbkCode.decodeGBK()
+            if (gbkCode.size == 2) {
+                println("ASCII编码(${asciiCode.toHexString(false, uppercase = true)}) 内容(${asciiText}) | Unicode编码(${uniCode.toHexString(false, uppercase = true)}) 内容(${uniText}) | GBK编码(${gbkCode.toHexString(false, uppercase = true)}) 内容(${gbkText})")
+            }
+        }
+    }
+
+    @Test
+    fun test5() {
+        val h = 0x81 .. 0xFE
+        val l = (0x40 .. 0xFE).filter { it != 0x7F }
+
+        for (i in h) {
+            for (j in l) {
+                val gbkCode = byteArrayOf(i.toByte(), j.toByte())
+                val gbkText = gbkCode.decodeGBK()
+                val uniCode = gbkText.encodeUnicode()
+                val uniText = uniCode.decodeUnicodeByByteArray()
+                val less = gbkCode.toUnsignedInt() - uniCode.toUnsignedInt()
+                println("GBK编码(${gbkCode.toHexString(false, uppercase = true)}) 内容(${gbkText}) | Unicode编码(${uniCode.toHexString(false, uppercase = true)}) 内容(${uniText}) | 差值(${less})")
+            }
+        }
+    }
+}
